8aec8fb26c5baf12b5c464a4020779e5377f6f77
Juseong39 <jskim@smartx.kr>
2020-02-25 22:12:48 +0900

    create project

---
+++ src/main/java/com/jojoldu/springwebserver/SpringWebserverApplication.java
@@ -0,0 +1,13 @@
+package com.jojoldu.springwebserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebserverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jojoldu/springwebserver/SpringWebserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jojoldu.springwebserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
